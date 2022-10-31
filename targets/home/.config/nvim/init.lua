471b741c4c5eb31c7184ec5c8f35b3b7ee00874d
Daniel Podwysocki <48068081+danielpodwysocki@users.noreply.github.com>
2022-10-31 21:58:56 +0100

    install coc and vim-polyglot

--- targets/home/.config/nvim/init.lua
+++ targets/home/.config/nvim/init.lua
@@ -3,6 +3,12 @@ local Plug = vim.fn['plug#']
 vim.call('plug#begin', '~/.config/nvim/plugged')
 
 Plug 'tpope/vim-fugitive'
+Plug 'github/copilot.vim'
+-- support for all the languages you can think of
+Plug 'sheerun/vim-polyglot'
+-- intellisense
+Plug 'neoclide/coc.nvim', {'branch': 'release'}
+
 
 vim.call('plug#end')
 
