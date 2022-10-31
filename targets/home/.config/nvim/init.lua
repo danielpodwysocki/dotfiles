b4f9a26ce38aefa7e7af6e617899c4e4a30a4007
Daniel Podwysocki <48068081+danielpodwysocki@users.noreply.github.com>
2022-10-31 21:42:09 +0100

    install vim-plug, add the plugin conf

--- targets/home/.config/nvim/init.lua
+++ targets/home/.config/nvim/init.lua
@@ -1,3 +1,12 @@
+local Plug = vim.fn['plug#']
+
+vim.call('plug#begin', '~/.config/nvim/plugged')
+
+Plug 'tpope/vim-fugitive'
+
+vim.call('plug#end')
+
+
 vim.opt.number = true
 -- don't highlight the last search, similar to running :noh
 vim.opt.hlsearch = false
