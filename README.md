82115f5ad3282471efd5ecda64021e43cad25659
Daniel Podwysocki <48068081+danielpodwysocki@users.noreply.github.com>
2022-11-05 22:32:28 +0100

    manage plugins using packer.nvim instead of VimPlug

--- README.md
+++ README.md
@@ -20,3 +20,31 @@ sudo dnf install epel-release
 sudo dnf install stow
 ```
 
+
+## neovim plugins
+To not keep those in source control, I use `packer.nvim`.
+(https://github.com/wbthomason/packer.nvim#quickstart)
+
+Common packer commands:
+```
+-- You must run this or `PackerSync` whenever you make changes to your plugin configuration
+-- Regenerate compiled loader file
+:PackerCompile
+
+-- Remove any disabled or unused plugins
+:PackerClean
+
+-- Clean, then install missing plugins
+:PackerInstall
+
+```
+
+### per-plugin setup instructions:
+
+GitHub Copilot:
+```
+:Copilot setup
+:Copilot help
+```
+
+
--- targets/home/.config/nvim/init.lua
+++ targets/home/.config/nvim/init.lua
@@ -1,18 +1,34 @@
-local Plug = vim.fn['plug#']
-
-vim.call('plug#begin', '~/.config/nvim/plugged')
-
-Plug 'tpope/vim-fugitive'
-Plug 'github/copilot.vim'
--- support for all the languages you can think of
-Plug 'sheerun/vim-polyglot'
--- intellisense
-Plug 'neoclide/coc.nvim'
+-- BOOTSTRAP PACKER --
 
 
-vim.call('plug#end')
+local vim = vim
+local execute = vim.api.nvim_command
+local fn = vim.fn
+-- ensure that packer is installed
+local install_path = fn.stdpath('data')..'/site/pack/packer/opt/packer.nvim'
+if fn.empty(fn.glob(install_path)) > 0 then
+    execute('!git clone https://github.com/wbthomason/packer.nvim '..install_path)
+    execute 'packadd packer.nvim'
+end
+vim.cmd('packadd packer.nvim')
+local packer = require'packer'
+local util = require'packer.util'
+packer.init({
+  package_root = util.join_paths(vim.fn.stdpath('data'), 'site', 'pack')
+})
+--- startup and add configure plugins
+packer.startup(function()
+  local use = use
+  -- add you plugins here like:
+  -- use 'neovim/nvim-lspconfig'
+  use 'tpope/vim-fugitive'
+  use 'sheerun/vim-polyglot'
+  use {'neoclide/coc.nvim' , branch = 'release'}
+  end
+)
 
 
+-- EDITOR OPTIONS --
 vim.opt.number = true
 -- don't highlight the last search, similar to running :noh
 vim.opt.hlsearch = false
