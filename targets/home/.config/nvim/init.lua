afd2a7672bae10546513fa47591ccfb52cf26a0a
Daniel Podwysocki <48068081+danielpodwysocki@users.noreply.github.com>
2022-12-07 17:25:28 +0100

    set up a status line

--- targets/home/.config/nvim/init.lua
+++ targets/home/.config/nvim/init.lua
@@ -23,11 +23,67 @@ packer.startup(function()
   use 'github/copilot.vim'
   use 'tpope/vim-fugitive'
   use 'sheerun/vim-polyglot'
+  use {
+  "nvim-neo-tree/neo-tree.nvim",
+    branch = "v2.x",
+    requires = { 
+      "nvim-lua/plenary.nvim",
+      "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
+      "MunifTanjim/nui.nvim",
+    }
+  }
+  use 'lewis6991/gitsigns.nvim'
   use {'neoclide/coc.nvim' , branch = 'release'}
   use 'davidhalter/jedi-vim'
   use 'deoplete-plugins/deoplete-jedi'
+  use {
+  'nvim-lualine/lualine.nvim',
+  requires = { 'kyazdani42/nvim-web-devicons', opt = true }
+  }
   end
 )
+
+-- StatusLine config --
+require('lualine').setup {
+  options = {
+    icons_enabled = true,
+    theme = 'auto',
+    component_separators = { left = '', right = ''},
+    section_separators = { left = '', right = ''},
+    disabled_filetypes = {
+      statusline = {},
+      winbar = {},
+    },
+    ignore_focus = {},
+    always_divide_middle = true,
+    globalstatus = false,
+    refresh = {
+      statusline = 1000,
+      tabline = 1000,
+      winbar = 1000,
+    }
+  },
+  sections = {
+    lualine_a = {'mode'},
+    lualine_b = {'branch', 'diff', 'diagnostics'},
+    lualine_c = {'filename'},
+    lualine_x = {'encoding', 'fileformat', 'filetype'},
+    lualine_y = {'progress'},
+    lualine_z = {'location'}
+  },
+  inactive_sections = {
+    lualine_a = {},
+    lualine_b = {},
+    lualine_c = {'filename'},
+    lualine_x = {'location'},
+    lualine_y = {},
+    lualine_z = {}
+  },
+  tabline = {},
+  winbar = {},
+  inactive_winbar = {},
+  extensions = {}
+}
 
 
 -- EDITOR OPTIONS --
